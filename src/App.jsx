9a4126c9363f049dd7f2a3137721aafef214dec5
orozcoding <orozcoding@gmail.com>
2025-02-17 22:51:46 -0400

    Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { Gallery } from './components/Gallery'
 import playBanner from '/play_store_banner.png'
 import appBanner from '/app_store_banner.svg'
+import urls from './constants/urls'
+import logo from '/logo.png'
 
 function App() {
 
@@ -9,12 +11,11 @@ function App() {
   return (
     <div className="text-black flex flex-col items-center justify-center gap-4">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1
-          className="text-4xl font-bold text-center"
-        >
+        <img className="w-20 rounded-md" src={logo} alt="logo" />
+        <h1 className="text-4xl font-bold text-center">
           Tasa Dolar Venezuela
         </h1>
-        <div className="text-center flex flex-col items-center justify-center gap-1">
+        <div className="text-center flex flex-col items-center justify-center">
           <span>Consulta la tasa del día</span>
           <span>Realiza cálculos en SEGUNDOS</span>
           <span>Arma solicitudes de pago</span>
@@ -22,8 +23,12 @@ function App() {
       </div>
 
       <div className="w-full flex gap-10 flex-wrap items-center justify-center">
-        <img className="w-40" src={playBanner} alt="play store banner" />
-        <img className="w-40" src={appBanner} alt="app store banner" />
+        <a href={urls.android} target="_blank" rel="noreferrer">
+          <img className="w-40" src={playBanner} alt="play store banner" />
+        </a>
+        <a href={urls.ios} target="_blank" rel="noreferrer">
+          <img className="w-40" src={appBanner} alt="app store banner" />
+        </a>
       </div>
 
       <div className="h-140">
---
+++ src/constants/urls.tsx
@@ -0,0 +1,6 @@
+const urls ={
+  'ios': 'https://apps.apple.com/ve/app/tasa-dolar-venezuela/id6741183039',
+  'android': 'https://play.google.com/store/apps/details?id=com.orozcoding.tasadolarapp',
+}
+
+export default urls
